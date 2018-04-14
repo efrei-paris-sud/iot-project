b65a069160ed92632ab2425a498630f575bf40d6
Enzo Besnainou <pi@raspberrypi>
2018-04-14 16:53:45 +0000

    personal file

---
+++ my-functions.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Optionally declare here custom functions/variables to use in commands and hooks
+# Prefix them in order to avoid conflicts
+# You can also source other external files
+read_file()
+{
+nl ~/liste.txt
+}
